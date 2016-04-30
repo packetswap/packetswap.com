de0d667de29677e311608de2db7dadc44d0133b4
Adriaan van Rossum <git@adriaan.io>
2016-04-30 18:18:19 +0200

    Make the read more button work

---
+++ js/app.js
@@ -0,0 +1,15 @@
+// Write forEach function because JavaScript still
+// does not support something great for this.
+var forEach = function(array, callback, scope) {
+  for (var i = 0; i < array.length; i++) {
+    callback.call(scope, i, array[i]);
+  }
+};
+
+// Make more button expand the sections
+var expands = document.querySelectorAll('section.expand');
+forEach(expands, function(index, section) {
+  section.addEventListener('click', function() {
+  	section.classList.add('open');
+  });
+});
