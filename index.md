f0422509caff81c99c5423ad990a8977e9801016
Adriaan van Rossum <git@adriaan.io>
2016-05-01 02:33:08 +0200

    Include image in header with an angle

--- index.md
+++ index.md
@@ -4,8 +4,13 @@
 <p>Koop en verkoop tweedehands producten met een druk op de knop zonder opgelicht te worden!</p>
 
 <section>
-  <p>PacketSwap wordt binnenkort gelanceerd en alleen jij krijgt de eerste bestelling verzendkosten vrij. Volg daarvoor de volgende stappen.</p>
+  <p>PacketSwap wordt binnenkort gelanceerd en jij krijgt de eerste bestelling verzendkosten vrij. Volg daarvoor de volgende stappen.</p>
 </section>
+
+<ul class="steps">
+  <li>Like jouw veilige marktplaats op Facebook om de lanceringsdatum niet te missen!</li>
+  <li>Like jouw moderne marktplaats op Twitter (als je dat hebt)</li>
+</ul>
 
 <section class="expand">
   <p>Er is veel mis met het doen van online tweedehands aankopen: je weet niet of je de verkoper kan vertrouwen, je kan een product niet terugsturen als je niet tevreden bent, je kan opgelicht worden, je moet een pakketje ophalen of je moet verzendkosten betalen.</p>
@@ -15,9 +20,4 @@
   <p>Voor al deze problemen gaan wij een oplossing bieden. Dit kunnen wij natuurlijk niet alleen, daarom zoeken we zo veel mogelijk gebruikers voor tijdens de lancering. Dus wil jij ons helpen door iets te koop te zetten op PacketSwap tijdens de lancering?</p>
 </section>
 
- Laat dan hier je e-mailadres achter:
-
-<input type="text" placeholder="Ja, vul 'm hier in!">
-(we zullen je absoluut niet spammen en uitschrijven kan altijd)
-
-<h2>Bedankt.</h2>
+<h3>Bedankt, je bent fantastisch!</h3>
--- js/app.js
+++ js/app.js
@@ -14,3 +14,61 @@ forEach(expands, function(index, section) {
     section.classList.remove('closed');
   });
 });
+
+// Facebook like button callback
+function facebookCallback() {
+  FB.Event.subscribe('edge.create', function() {
+    goTo('twitter');
+  });
+}
+
+var facebookLoaded = false;
+document.body.addEventListener('DOMSubtreeModified', function () {
+  if (typeof FB !== 'undefined' && !facebookLoaded) {
+  	facebookLoaded = true;
+  	facebookCallback();
+  }
+}, false);
+
+// Go to next page
+var expands = document.querySelectorAll('[data-next]');
+forEach(expands, function(index, element) {
+  element.addEventListener('click', function(element) {
+    var next = element.target.getAttribute('data-next');
+    goTo(next);
+  });
+});
+
+function goTo(step) {
+  document.querySelector('html').classList.add('hide-main');
+  ul.style.display = 'block';
+  var ul = document.querySelector('ul.fullscreen');
+  if (step === 'twitter') {
+    ul.querySelector('li.facebook').classList.add('done');
+    ul.querySelector('li.facebook').classList.remove('active');
+    ul.querySelector('li.twitter').classList.add('active');
+  }
+  else if (step === 'email') {
+    ul.querySelector('li.twitter').classList.add('done');
+    ul.querySelector('li.twitter').classList.remove('active');
+    ul.querySelector('li.email').classList.add('active');
+  }
+  else if (step === 'done') {
+    ul.style.display = 'none';
+    document.querySelector('html').classList.remove('hide-main');
+  }
+}
+
+twttr.ready(
+  function (twttr) {
+    twttr.events.bind(
+      'follow',
+      function (event) {
+        console.log(event.data);
+        var followedUserId = event.data.user_id;
+        var followedScreenName = event.data.screen_name;
+      }
+    );
+  }
+);
+
